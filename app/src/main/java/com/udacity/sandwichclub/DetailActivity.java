9ae29543a317f022122314740804c6ff02bee8e2
Asmaathabet <asmathabet2009@hotmail.com>
2018-09-07 18:40:24 +0300

    FirstCommit

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,9 +1,13 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,11 +19,19 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    TextView also_known_Detial;
+    TextView origin_Detial;
+    TextView ingredients_Detial;
+    TextView description_Detial;
+
+    Sandwich sandwich;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        RUNDetailsIntent();
         ImageView ingredientsIv = findViewById(R.id.image_iv);
 
         Intent intent = getIntent();
@@ -50,13 +62,54 @@ public class DetailActivity extends AppCompatActivity {
 
         setTitle(sandwich.getMainName());
     }
+    private void RUNDetailsIntent() {
+        also_known_Detial  = findViewById(R.id.also_known_tv);
+        origin_Detial      = findViewById(R.id.origin_tv);
+        ingredients_Detial = findViewById(R.id.ingredients_tv);
+        description_Detial = findViewById(R.id.description_tv);
+
+    }
 
     private void closeOnError() {
         finish();
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
+
     private void populateUI() {
+
+        if (sandwich.getAlsoKnownAs().isEmpty() != true ){
+
+            also_known_Detial.setText(TextUtils.join("",sandwich.getAlsoKnownAs()));
+
+        }else {
+            also_known_Detial.setText("Null");
+        }
+
+        if (sandwich.getPlaceOfOrigin().isEmpty()){
+
+            origin_Detial .setText("Null");
+        }else {
+
+            origin_Detial.setText(sandwich.getPlaceOfOrigin());
+
+        }
+
+
+        if ( sandwich.getIngredients().isEmpty() != true){
+            ingredients_Detial.setText(TextUtils.join("",sandwich.getIngredients()));
+
+        }else {
+            ingredients_Detial.setText("Null");
+        }
+
+        if (sandwich.getDescription().isEmpty() != true){
+
+            description_Detial.setText(sandwich.getDescription());
+        }else {
+            description_Detial.setText("Null");
+
+        }
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,98 @@
 package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class JsonUtils {
+    public static final String MAIN_NAME ="mainName";
+    public static final String ALSO_KNOWN_AS ="alsoKnownAs";
+    public static final String PLACE_OF_ORIGIN ="placeOfOrigin";
+    public static final String DESCRIPTION ="description";
+    public static final String IMAGE ="image";
+    public static final String INGREDIENTS ="ingredients";
+
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = new Sandwich();
+
+        try {
+
+            // For  Main Name Field (String)
+            JSONObject sandwichObject = new JSONObject(json);
+            JSONObject mainName = sandwichObject.getJSONObject(MAIN_NAME);
+            if (mainName == null) {
+                return null;
+            }
+            sandwich.setMainName(mainName.optString(MAIN_NAME));
+
+            // For  Also Known As Field (Array)
+            JSONArray AlsoKnownAs = mainName.optJSONArray(ALSO_KNOWN_AS);
+            if (AlsoKnownAs == null) {
+                return null;
+            }
+            sandwich.setAlsoKnownAs(getStringsFromArrayJSON(AlsoKnownAs));
+
+            // For Place of Origin
+            JSONObject placeOfOrigin = sandwichObject.getJSONObject(PLACE_OF_ORIGIN);
+            if (placeOfOrigin == null) {
+                return null;
+            }
+            sandwich.setPlaceOfOrigin(placeOfOrigin.optString(PLACE_OF_ORIGIN));
+
+            // For Description
+            JSONObject Description = sandwichObject.getJSONObject(DESCRIPTION);
+            if (Description == null) {
+                return null;
+            }
+            sandwich.setDescription(Description.optString(DESCRIPTION));
+
+            //For Image
+            JSONObject Image = sandwichObject.getJSONObject(IMAGE);
+            if (Image == null) {
+                return null;
+            }
+            sandwich.setImage(Image.optString(IMAGE));
+
+            //For Ingredients (Array)
+            JSONArray Ingredients = mainName.optJSONArray(INGREDIENTS);
+            if (Ingredients == null) {
+                return null;
+            }
+            sandwich.setIngredients(getStringsFromArrayJSON(Ingredients));
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return sandwich;
+    }
+
+    private static List<String> getStringsFromArrayJSON(JSONArray ArrayFromJSON) {
+        List<String> ArrayDetails =new ArrayList<>();
+
+        if (ArrayFromJSON != null) {
+            for (int i = 0; i < ArrayFromJSON.length()-1 ; i++) {
+
+                String arrayString = null;
+                try {
+                    arrayString = ArrayFromJSON.getString(i);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                ArrayDetails.add(arrayString);
+
+            }
+
+        }
+
+        return ArrayDetails;
     }
 }
